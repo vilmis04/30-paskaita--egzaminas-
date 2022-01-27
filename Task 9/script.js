64086d5541892ed5a3aa41350e47f75140695268
Vilmantas <vilmantas.sudaris@gmail.com>
2022-01-27 20:43:19 +0200

    Task 9 completed

--- Task 9/script.js
+++ Task 9/script.js
@@ -6,3 +6,14 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kiru atveju false 
 ------------------------------------------------------------------------------------------------------ */
+class Movie {
+    constructor(title, director, budget) {
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+
+    wasExpensive() {
+        return this.budget>100000000;
+    }
+}
